f98026ba07c01b31b4d60c8a05b541ebbb93c6a4
IceMints <51943194+IceMints@users.noreply.github.com>
2021-09-02 15:24:37 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 flask db migrate
 flask db upgrade
-gunicorn --worker-class eventlet -w 1 --bin 0.0.0.0:80 wsgi:app --capture-output --log-level debug
+
+if [ "$FLASK_ENV" = "development"]
+then
+    echo "Running Flask Development Server"
+    flask run --host=0.0.0.0 --port=80
+else
+    gunicorn --worker-class eventlet -w 1 --bin 0.0.0.0:80 wsgi:app --capture-output --log-level debug
+fi
+
 
